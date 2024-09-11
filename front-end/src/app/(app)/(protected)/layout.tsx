af43fc8f1755da2bc948edb29dcc447411624e20
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-11 00:34:11 -0300

    middleware

--- front-end/src/app/(app)/(protected)/layout.tsx
+++ front-end/src/app/(app)/(protected)/layout.tsx
@@ -1,10 +1,5 @@
-import { isAuthenticated } from '@/auth'
-import { redirect } from 'next/navigation'
-
 import { ProtectedSidebar } from './sidebar'
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-  if (!isAuthenticated()) return redirect('/sign-in')
-
   return <ProtectedSidebar>{children}</ProtectedSidebar>
 }
---
+++ front-end/src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse, type NextRequest } from 'next/server'
+
+import * as auth from './auth'
+
+const authPrefixes = ['/sign-in', '/sign-up', '/forgot-password']
+const protectedPrefixes = ['/profile', '/chat']
+
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+  const isAuthenticated = auth.isAuthenticated()
+
+  // Redirect to home if authenticated user tries to access auth routes
+  if (isAuthenticated && authPrefixes.some(prefix => pathname.startsWith(prefix))) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // Redirect to sign-in if unauthenticated user tries to access protected routes
+  if (!isAuthenticated && protectedPrefixes.some(prefix => pathname.startsWith(prefix))) {
+    return NextResponse.redirect(new URL('/sign-in', req.url))
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|logo.svg|sitemap.xml|robots.txt).*)'
+  ]
+}
