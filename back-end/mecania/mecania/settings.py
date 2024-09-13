91ec6f3f1c3edc00b6f9c506fb3a88b0a0d48c06
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-13 01:03:55 -0300

    env

--- back-end/mecania/mecania/settings.py
+++ back-end/mecania/mecania/settings.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+from dotenv import load_dotenv
 from utils.env import env
 from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = BASE_DIR.parent / ".env"
+
+if dotenv_path.exists():
+    load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
--- back-end/mecania/utils/env.py
+++ back-end/mecania/utils/env.py
@@ -1,9 +1,5 @@
-import os
-from dotenv import load_dotenv
+from os import getenv
 from .assertions import is_primitive_class
-
-# Load the .env file
-load_dotenv()
 
 
 def env(key: str, default=None, type=None, split: str = None, validate_split=None):
@@ -11,12 +7,12 @@ def env(key: str, default=None, type=None, split: str = None, validate_split=Non
         raise ValueError("type must be a primitive type like int, float, bool, or str")
 
     if isinstance(default, bool) or type == bool:
-        return os.environ.get(key, str(default)) == "True"
+        return getenv(key, str(default)) == "True"
 
     if type is not None:
-        return type(os.environ.get(key, default))
+        return type(getenv(key, default))
 
-    value = os.environ.get(key, default)
+    value = getenv(key, default)
 
     if split is not None:
         return [item for item in value.split(split) if item and (validate_split is None or validate_split(item))]
