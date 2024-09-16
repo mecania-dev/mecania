5ede43bafae411b82016f0b1b95b2abbcacecca1
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-16 18:07:32 -0300

    noStore

--- front-end/src/app/api/keep-alive/route.ts
+++ front-end/src/app/api/keep-alive/route.ts
@@ -1,7 +1,9 @@
 import { api } from '@/http'
+import { unstable_noStore as noStore } from 'next/cache'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  noStore()
   const res = await api.get('keep-alive', { raw: true })
   return NextResponse.json(res.data, { status: res.status })
 }
