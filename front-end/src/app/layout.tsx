2034977c77eee73a979a7afcd6f2aa810dafcad7
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-18 01:43:50 -0300

    scrollbar

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -27,7 +27,7 @@ export const maxDuration = 60
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${inter.className} antialiased`} suppressHydrationWarning>
-      <body className="flex min-h-[100dvh] flex-col overflow-auto scroll-smooth bg-default-50 text-foreground scrollbar-thumb-active-secondary/75 scrollbar-thumb-default-200 scrollbar-thumb-hover-default-300 scrollbar-track-default-50">
+      <body className="flex min-h-[100dvh] flex-col overflow-auto scroll-smooth bg-default-50 text-foreground scrollbar-h-1.5 scrollbar-w-1.5 scrollbar-thumb-active-secondary/50 scrollbar-thumb-default-300 scrollbar-thumb-hover-default-400 scrollbar-track-transparent">
         <Providers>{children}</Providers>
       </body>
     </html>
