6d6b85cb8ba3677b67c36038e3a2f3b5ca17b780
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-18 10:23:23 -0300

    static error fixed

--- back-end/mecania/settings.py
+++ back-end/mecania/settings.py
@@ -178,11 +178,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 if DEVELOPMENT:
-    STATIC_URL = "static/"
-    STATIC_ROOT = BASE_DIR / "static"
-    MEDIA_URL = "media/"
-    MEDIA_ROOT = BASE_DIR / "media"
     STORAGES = {
         "default": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
         "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
