0c0a65a7a8bbd7c3bedf1eb6ea140c5ac03ec8b9
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-23 21:44:42 -0300

    is loading on register mechanics

--- front-end/src/app/(app)/(protected)/mechanics/register/register-mechanics-list.tsx
+++ front-end/src/app/(app)/(protected)/mechanics/register/register-mechanics-list.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from '@/components/button'
+import { useIsLoading } from '@/hooks/use-is-loading'
 import { createMechanic } from '@/http/user/create-mechanic'
 
 import { NewMechanicCard } from './register-form/new-mechanic-card'
@@ -8,21 +9,22 @@ import { useRegisterMechanics } from './register-form/use-register-mechanics'
 
 export function RegisterMechanicsList() {
   const { mechanics, removeMechanic } = useRegisterMechanics()
-
-  async function onRegisterMechanics() {
+  const [onRegisterMechanics, isLoading] = useIsLoading(async () => {
     for (const mechanic of mechanics) {
       const res = await createMechanic(mechanic)
       if (res.ok) {
         removeMechanic(mechanics.indexOf(mechanic))
       }
     }
-  }
+  })
 
   if (mechanics.length === 0) return null
 
   return (
     <div className="space-y-4">
-      <Button onPress={onRegisterMechanics}>Cadastrar oficinas</Button>
+      <Button onPress={onRegisterMechanics} isLoading={isLoading}>
+        Cadastrar oficinas
+      </Button>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {mechanics.map((mechanic, index) => (
           <NewMechanicCard index={index} mechanic={mechanic} key={index} />
