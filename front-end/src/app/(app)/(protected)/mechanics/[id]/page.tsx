32db2b2125db913a5654527e011a4407fea393aa
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-23 21:00:10 -0300

    mechanic generateMetadata

--- front-end/src/app/(app)/(protected)/mechanics/[id]/page.tsx
+++ front-end/src/app/(app)/(protected)/mechanics/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from '@/auth'
 import { getUser } from '@/http'
 import { Metadata } from 'next'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
 import { MechanicDetails } from '.'
 
@@ -12,7 +12,8 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const res = await getUser(Number(params.id))
-  if (!res.ok) return notFound()
+  if (res.status === 404) return notFound()
+  if (!res.ok) return redirect('/profile')
   return {
     title: `Oficina ${res.data.username}`
   }
