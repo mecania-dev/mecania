08fc85b4cc15501464ac4213a84addefc5c4afb3
RaulSCoelho <raul.s.coelho@gmail.com>
2024-09-25 17:55:19 -0300

    mechanics

--- front-end/src/app/(app)/(protected)/mechanics/index.tsx
+++ front-end/src/app/(app)/(protected)/mechanics/index.tsx
@@ -3,38 +3,35 @@
 import { FaTools } from 'react-icons/fa'
 
 import { AsyncScroll } from '@/components/async-scroll'
-import { useSWRCustom } from '@/hooks/swr/use-swr-custom'
 import { Mechanic } from '@/types/entities/mechanic'
 import { range } from 'lodash'
 
 import { MechanicCard } from './mechanic-card'
 
 export function MechanicsList() {
-  const { state } = useSWRCustom<Mechanic[]>(`users/mechanics/`)
-  const mechanics = state.data
-  const isEmpty = !state.isLoading && (!mechanics || mechanics.length === 0)
-
-  if (isEmpty) {
-    return (
-      <div className="flex flex-col items-center justify-center space-y-4 p-10 text-center">
-        <FaTools className="animate-pulse text-6xl text-default-400" />
-        <p className="text-large font-medium text-default-600">Nenhuma oficina cadastrada.</p>
-        <p className="text-default-500">Assim que novas oficinas forem cadastradas, você poderá vê-las aqui.</p>
-      </div>
-    )
-  }
-
   return (
     <AsyncScroll<Mechanic> url="users/mechanics/" className="grow">
-      {(mechanics, isLoading) => (
-        <div className="grid grid-cols-1 gap-6 p-5 md:grid-cols-2 xl:grid-cols-3">
-          {isLoading
-            ? range(10).map(i => <MechanicCard key={i} isLoaded={!isLoading} />)
-            : mechanics?.map(mechanic => (
-                <MechanicCard mechanic={mechanic} href={`/mechanics/${mechanic.id}`} key={mechanic.id} />
-              ))}
-        </div>
-      )}
+      {(mechanics, isLoading) => {
+        if (!isLoading && !mechanics?.length) {
+          return (
+            <div className="flex flex-col items-center justify-center space-y-4 p-10 text-center">
+              <FaTools className="animate-pulse text-6xl text-default-400" />
+              <p className="text-large font-medium text-default-600">Nenhuma oficina cadastrada.</p>
+              <p className="text-default-500">Assim que novas oficinas forem cadastradas, você poderá vê-las aqui.</p>
+            </div>
+          )
+        }
+
+        return (
+          <div className="grid grid-cols-1 gap-6 p-5 md:grid-cols-2 xl:grid-cols-3">
+            {isLoading
+              ? range(10).map(i => <MechanicCard key={i} isLoaded={!isLoading} />)
+              : mechanics?.map(mechanic => (
+                  <MechanicCard mechanic={mechanic} href={`/mechanics/${mechanic.id}`} key={mechanic.id} />
+                ))}
+          </div>
+        )
+      }}
     </AsyncScroll>
   )
 }
