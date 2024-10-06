8394d5068888bf04664275897d6215c4b2662676
RaulSCoelho <raul.s.coelho@gmail.com>
2024-10-06 02:29:54 -0300

    message loading

--- front-end/src/app/(app)/(protected)/chat/chat-window.tsx
+++ front-end/src/app/(app)/(protected)/chat/chat-window.tsx
@@ -2,6 +2,7 @@
 
 import { HiOutlineSparkles } from 'react-icons/hi2'
 
+import Loading from '@/app/loading'
 import { Button } from '@/components/button'
 import { ChatMessage } from '@/components/chat/message'
 import { ChatWindow } from '@/components/chat/window'
@@ -12,38 +13,49 @@ import { ChatRecommendations } from './recommendations'
 import { useChat } from './use-chat'
 import { VehicleSelector } from './vehicle-selector'
 
-export function AIChatWindow() {
+interface AIChatWindowProps {
+  isLoading: boolean
+}
+
+export function AIChatWindow({ isLoading }: AIChatWindowProps) {
   const { user } = useUser()
-  const { chat, messages, recommendations } = useChat()
+  const { chat, messages, recommendations, getCurrentQuestion } = useChat()
+  const { isAllAnswered } = getCurrentQuestion()
 
   return (
     <ChatWindow>
       <p className="mb-20"></p>
-      <VehicleSelector />
-      <InitialQuestions />
-      {messages.map(msg => (
-        <ChatMessage
-          isSender={user?.id === msg.sender.id}
-          isAIGenerating={msg.sender.isAi && !msg.content}
-          key={msg.id}
-          {...msg}
-        />
-      ))}
-      {chat?.issues.some(i => i.recommendations.length > 0) && (
-        <div className="!my-10 animate-appearance-in text-center">
-          <Button
-            size="lg"
-            data-modal-open={recommendations.isModalOpen}
-            className="bg-gradient-to-tr from-primary via-[#CDCDCD] to-secondary font-bold text-default-50 shadow-lg"
-            startContent={
-              <HiOutlineSparkles className="h-6 w-6 shrink-0 rotate-0 animate-[spinner-spin_0.8s_ease] transition-transform duration-500 group-data-[modal-open=true]:rotate-[360deg]" />
-            }
-            onPress={recommendations.openModal}
-          >
-            Oficinas
-          </Button>
-          <ChatRecommendations />
-        </div>
+      {isLoading && !isAllAnswered ? (
+        <Loading isFixed={false} />
+      ) : (
+        <>
+          <VehicleSelector />
+          <InitialQuestions />
+          {messages.map(msg => (
+            <ChatMessage
+              isSender={user?.id === msg.sender.id}
+              isAIGenerating={msg.sender.isAi && !msg.content}
+              key={msg.id}
+              {...msg}
+            />
+          ))}
+          {chat?.issues.some(i => i.recommendations.length > 0) && (
+            <div className="!my-10 animate-appearance-in text-center">
+              <Button
+                size="lg"
+                data-modal-open={recommendations.isModalOpen}
+                className="bg-gradient-to-tr from-primary via-[#CDCDCD] to-secondary font-bold text-default-50 shadow-lg"
+                startContent={
+                  <HiOutlineSparkles className="h-6 w-6 shrink-0 rotate-0 animate-[spinner-spin_0.8s_ease] transition-transform duration-500 group-data-[modal-open=true]:rotate-[360deg]" />
+                }
+                onPress={recommendations.openModal}
+              >
+                Oficinas
+              </Button>
+              <ChatRecommendations />
+            </div>
+          )}
+        </>
       )}
     </ChatWindow>
   )
