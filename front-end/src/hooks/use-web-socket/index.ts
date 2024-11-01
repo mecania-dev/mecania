86b364ac7805c11ef11e4a8e95cd545219c51558
RaulSCoelho <raul.s.coelho@gmail.com>
2024-11-01 02:43:31 -0300

    websocket retry

--- front-end/src/hooks/use-web-socket/index.ts
+++ front-end/src/hooks/use-web-socket/index.ts
@@ -4,6 +4,8 @@ import { env } from '@/env'
 import { getCookie } from 'cookies-next'
 
 export interface WebSocketProps<T = any> {
+  maxRetries?: number
+  retryDelay?: number
   isDisabled?: boolean
   onMessage?: (data: T) => void
   onError?: (e: Event) => void
@@ -13,43 +15,60 @@ export interface WebSocketProps<T = any> {
 
 export function useWebSocket<T = any>(
   url: string,
-  { isDisabled, onMessage, onError, onOpen, onClose }: WebSocketProps<T> = {}
+  { maxRetries = 5, retryDelay = 1000, isDisabled, onMessage, onError, onOpen, onClose }: WebSocketProps<T> = {}
 ) {
   const socketRef = useRef<WebSocket | null>(null)
   const [isConnected, setIsConnected] = useState(false)
+  const [retryCount, setRetryCount] = useState(0)
 
   useEffect(() => {
     if (isDisabled) return
-    const fullUrl = new URL(env.NEXT_PUBLIC_API_BASE_URL?.replace('/api', '') + url)
-    fullUrl.searchParams.append('token', getCookie('access_token') ?? '')
-    const socket = new WebSocket(fullUrl.href.replace('http', 'ws'))
 
-    socket.onopen = e => {
-      setIsConnected(true)
-      if (onOpen) onOpen(e)
+    const connectWebSocket = () => {
+      const fullUrl = new URL(env.NEXT_PUBLIC_API_BASE_URL?.replace('/api', '') + url)
+      fullUrl.searchParams.append('token', getCookie('access_token') ?? '')
+      const socket = new WebSocket(fullUrl.href.replace('http', 'ws'))
+
+      socket.onopen = e => {
+        if (onOpen) onOpen(e)
+        setRetryCount(0) // Reset retry count on successful connection
+        !isConnected && setIsConnected(true)
+      }
+
+      socket.onmessage = e => {
+        const data = JSON.parse(e.data)
+        if (onMessage) onMessage(data)
+      }
+
+      socket.onerror = e => {
+        if (onError) onError(e)
+      }
+
+      socket.onclose = e => {
+        if (onClose) onClose(e)
+
+        // Retry logic
+        if (!isConnected && retryCount < maxRetries) {
+          setRetryCount(prevCount => prevCount + 1)
+          const timeout = Math.min(retryDelay * Math.pow(2, retryCount), 30000) // Exponential backoff
+          setTimeout(connectWebSocket, timeout)
+        }
+
+        isConnected && setIsConnected(false)
+      }
+
+      socketRef.current = socket
     }
 
-    socket.onmessage = e => {
-      const data = JSON.parse(e.data)
-      if (onMessage) onMessage(data)
-    }
-
-    socket.onerror = e => {
-      if (onError) onError(e)
-    }
-
-    socket.onclose = e => {
-      setIsConnected(false)
-      if (onClose) onClose(e)
-    }
-
-    socketRef.current = socket
+    connectWebSocket()
 
     return () => {
-      socket.close()
+      if (socketRef.current) {
+        socketRef.current.close()
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isDisabled, url])
+  }, [isDisabled, url, retryCount, maxRetries, retryDelay])
 
   function sendMessage<M = any>(message: M) {
     if (socketRef.current && isConnected) {
