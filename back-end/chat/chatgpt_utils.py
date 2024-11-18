b73d837007c9c9185b2ffff0641b9367bb46d52d
RaulSCoelho <raul.s.coelho@gmail.com>
2024-11-17 23:49:29 -0300

    additional_instructions

--- back-end/chat/chatgpt_utils.py
+++ back-end/chat/chatgpt_utils.py
@@ -22,7 +22,7 @@ async def ask_gpt(message: str):
         await client.beta.threads.runs.create_and_poll(
             thread_id=thread.id,
             assistant_id=env("ASSISTANT_ID"),
-            additional_instructions="IMPORTANTE: Lembre de sempre buscar os arquivos anexados antes de indicar recomendações, mantendo o nome exatamente igual ao do arquivo anexado. Em hipótese alguma, indique recomendações sem antes verificar os arquivos anexados.",
+            additional_instructions="IMPORTANTE: Lembre-se de sempre buscar os arquivos anexados antes de indicar recomendações, mantendo o nome exatamente igual ao do arquivo anexado. Em hipótese alguma, indique recomendações sem antes verificar os arquivos anexados. No arquivo contém um array de categorias, onde você buscará as informações. Desse array fornecido nos arquivos anexados, você pegará o valor em 'category' e colocará no campo 'category' do issue, e no array 'recommendations' do issue, você pegará o valor em 'name' do array 'services' do arquivo anexado.",
         )
     except Exception as e:
         await database_sync_to_async(logger.error)(
