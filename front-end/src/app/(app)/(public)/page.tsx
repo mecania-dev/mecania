03a8b6688f7c955b713d62b8a75c23b7462ccc44
RaulSCoelho <raul.s.coelho@gmail.com>
2024-11-22 19:53:52 -0300

    home screen

--- front-end/src/app/(app)/(public)/page.tsx
+++ front-end/src/app/(app)/(public)/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import { Button } from '@/components/button'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div className="p-5">
-      <h1 className="text-center text-2xl font-bold">Home</h1>
+    <div className="flex min-h-[calc(100dvh-64px)] flex-col items-center justify-center bg-gradient-to-b from-primary-50 via-white to-primary-100 text-default-800">
+      <main className="mt-16 flex flex-col items-center space-y-8 px-5 text-center md:px-10">
+        <h1 className="text-4xl font-bold leading-tight text-primary md:text-6xl">
+          Bem-vindo ao{' '}
+          <span className="text-secondary">
+            MECAN<span className="text-primary">IA</span>
+          </span>
+        </h1>
+        <p className="max-w-3xl text-lg text-default-600 md:text-xl">
+          O app que conecta motoristas a mecânicos de confiança e simplifica a manutenção do seu veículo.
+        </p>
+        <Button as={Link} radius="full" size="lg" color="secondary" href="/sign-in">
+          Explore Agora
+        </Button>
+      </main>
     </div>
   )
 }
